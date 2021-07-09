31941583092633bb6cb3f3612c7121aa20c094bf
825847@BIGDECISIONS.COGNIZANT.COM <825847@BIGDECISIONS.COGNIZANT.COM>
2021-07-09 12:18:18 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Build Projects
+          Learn React and Build Projects..
         </a>
       </header>
     </div>
